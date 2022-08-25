2e59e05929d46ffca5437b94498210bd4a92e1b0
[아이앤소프트]정지운 <jjw5050@hotmail.com>
2022-08-25 13:03:49 +0900

    lecture: QueryProjection

--- src/main/java/study/querydsl/dto/MemberDto.java
+++ src/main/java/study/querydsl/dto/MemberDto.java
@@ -1,5 +1,6 @@
 package study.querydsl.dto;
 
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,8 @@ public class MemberDto {
     private String username;
     private int age;
 
+    //QueryDSL 에 대한 의존성이 생긴다
+    @QueryProjection
     public MemberDto(String username, int age) {
         this.username = username;
         this.age = age;
